a03c451b64bc68214ba16d0dbb0763abfc6e6953
adarsh <adarshjithu10@gmail.com>
2025-05-09 16:19:17 +0530

    tailwind.config.ts file edited

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,22 @@
-import type { Config } from "tailwindcss";
-
-export default {
+// // tailwind.config.js
+// module.exports = {
+//   darkMode:'class',
+//   content: [
+//     "./src/**/*.{html,js,ts,jsx,tsx}", // adjust based on your structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+// tailwind.config.js
+module.exports = {
+  darkMode:'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}", // adjust based on your structure
   ],
-  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      screens: {
-        sm: "640px", // Small devices (e.g., phones)
-        md: "768px", // Medium devices (e.g., tablets)
-        lg: "1024px", // Large devices (e.g., laptops/desktops)
-        xl: "1280px", // Extra large devices (e.g., wide desktops)
-        "2xl": "1536px", // 2x Extra large devices
-        custom: "1440px", // Example custom breakpoint
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
